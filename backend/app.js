ee8991a11e1b97723d52e62fbf17524289b5ab18
Winfred Huang <huang.winfred21@gmail.com>
2021-05-15 23:46:03 -0700

    Setup api routes

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,20 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+const users = require("./routes/api/users");
+const courses = require("./routes/api/courses");
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/courses", courses);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
