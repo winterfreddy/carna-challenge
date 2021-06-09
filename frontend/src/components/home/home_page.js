6180db107c466256a9d348a714c44400b6a56a94
Winfred Huang <huang.winfred21@gmail.com>
2021-06-08 22:23:07 -0700

    Setup part of redux state management

---
+++ frontend/src/components/home/home_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+
+    render() {
+        return (
+        <div>
+            <h1>Carna Mock Academics</h1>
+            <footer>
+                Copyright &copy; 2021 Carna
+            </footer>
+        </div>
+        );
+    }
+}
+
+export default HomePage;
